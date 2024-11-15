6043b8a4ad266f112c251e815eaeaade31265a7a
Lucas <lucassguilherme159@gmail.com>
2024-11-15 17:34:25 -0400

    Iniciando métodos úteis de string

---
+++ 1.PrimeiroPassosComJavaScript/06.TrabalhandoComTexto/strings.js
@@ -0,0 +1,14 @@
+
+let testeStrings = "Eu quero que aspas \"\" escapem ";
+console.log(testeStrings);
+
+// converter string em número
+let myString = "123";
+let myNum = Number(myString);
+console.log(typeof myNum); //number
+
+//converter número em string
+myString = myNum.toString();
+console.log(typeof myString); //string
+
+
