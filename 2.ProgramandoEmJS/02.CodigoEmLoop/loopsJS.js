7e1f2734bd0dd794dcbcc45b7d8a590c183341dd
Lucas <lucassguilherme159@gmail.com>
2024-11-19 17:30:53 -0400

    falta fazer 2 aprendizado ativo

--- 2.ProgramandoEmJS/02.CodigoEmLoop/loopsJS.js
+++ 2.ProgramandoEmJS/02.CodigoEmLoop/loopsJS.js
@@ -1,3 +1,26 @@
 
+//exemplo continue
 
-  
+for (let i = 0; i <= 10; i++) {
+    if (i % 2 === 0) { 
+        continue; // Se "i" for par, pula para a próxima iteração
+    }
+    console.log(i); // Só imprime números ímpares
+}
+
+//while
+
+const cats = ['Bill', 'Jeff', 'Pete', 'Biggles', 'Jasmin'];
+let info = 'My cats are called ';
+var i = 0;
+
+while (i < cats.length) {
+  if (i === cats.length - 1) {
+    info += "and " + cats[i] + ".";
+  } else {
+    info += cats[i] + ", ";
+  }
+
+  i++;
+  console.log(info)
+}
