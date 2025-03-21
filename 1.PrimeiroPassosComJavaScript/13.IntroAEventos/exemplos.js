9abe86f70739c3435571da9c9f68c402006699be
Lucas-S-Guilherme <111232722+Lucas-S-Guilherme@users.noreply.github.com>
2025-03-21 17:29:38 +0000

    Continuando - Eventos

--- 1.PrimeiroPassosComJavaScript/13.IntroAEventos/exemplos.js
+++ 1.PrimeiroPassosComJavaScript/13.IntroAEventos/exemplos.js
@@ -10,3 +10,24 @@ btn.onclick = function () { //manipulador de eventos onclick
     "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
     document.body.style.backgroundColor = rndCol;
 };
+
+//Exemplo acima reescrito, utilizando addEventListener() e removeEventListener()
+
+var btn = document.querySelector("button");
+
+function bgChange() {
+    var rndCol = 
+    "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+    document.body.style.backgroundColor = rndCol;
+}
+
+btn.addEventListener("click", bgChange); // 2 parâmetros, o nome do evento para o qual registramos esse manipulador, e o código que compreende a função do manipulador que queremos executar em resposta a ele (bgChange)
+
+//Também é apropriada colocar todo o código denrto da função addEventListener(), em uma função anônima:
+
+btn.addEventListener("click", function (){
+    var rndCol = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+    document.body.style.backgroundColor = rndCol;
+});
+
+
